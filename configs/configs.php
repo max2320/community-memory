7f0169fd0e7a0bf90102cf74f7e5a99ab32b356d
LaryRocha <lary_web@hotmail.com>
2015-11-10 18:20:43 -0200

    lary

--- configs/configs.php
+++ configs/configs.php
@@ -3,8 +3,8 @@ return [
 	# DATABASE
 		'db_dsn' => "mysql:dbname=community_memory;host=localhost;port=3306",
 		'db_user' => "root",
-		// 'db_password' => "", Larissa
-		'db_password' => "max2320hk",
+		'db_password' => "", //Larissa
+		//'db_password' => "max2320hk",
 	# END DATABASE
 	# ROUTES
 		'route_root' => 'auth/login',
