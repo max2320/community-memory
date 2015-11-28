89b0b3ee673cd4ad5f664c2f83a339e31fe9b661
LaryRocha <lary_web@hotmail.com>
2015-11-27 23:16:30 -0200

    Like Comments

--- view/comment/index.php
+++ view/comment/index.php
@@ -11,6 +11,25 @@ foreach ($post->comments() as $comment) {
  				<div class="caption">
           <div><?= $comment->content; ?> </div>
    				<div><?= Date::formatToShow($comment->date_time, true); ?> </div>
+   				<div>
+              <?php 
+              if($comment->userLike($_SESSION['session_user_id'])){
+                ?>
+                <a href="/comment/dislike/?comment_id=<?php echo $comment->_id; ?>" class="btn btn-primary" role="button">
+                  Descurtir 
+                  <span class="badge"><?php echo $comment->likeCount(); ?></span>
+                </a>
+                <?php
+              }else{
+                ?>
+                <a href="/comment/like/?comment_id=<?php echo $comment->_id; ?>" class="btn btn-primary" role="button">
+                  Curtir 
+                  <span class="badge"><?php echo $comment->likeCount(); ?></span>
+                </a>
+                <?php
+              }
+              ?>
+            <div>
   			</div>
 			</div>
 		</div>
