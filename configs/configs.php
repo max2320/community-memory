50234624b5c1835117c9403bf69db20e1aa367c9
LaryRocha <lary_web@hotmail.com>
2015-11-29 21:41:02 -0200

    Config Lary

--- configs/configs.php
+++ configs/configs.php
@@ -4,6 +4,7 @@ return [
 		'db_dsn' => "mysql:dbname=community_memory;host=localhost;port=3306",
 		'db_user' => "root",
 		'db_password' => "max2320hk",
+		//'db_password' => "",
 	# END DATABASE
 	# ROUTES
 		'route_root' => 'auth/login',
@@ -15,7 +16,7 @@ return [
 	# SMTP SETTINGS // Conecta ao servidor, e envia o email para continuar preencher o perfil.
 		'smtp_host' => 'smtp.domain.com',
 		'smtp_username' => 'teste@maxfs.com',
-		'smtp_password' => '',
+		'smtp_password' => 'Larissa1234',
 		'smtp_port' => 465,
 		'smtp_security' => 'ssl', // tsl or ssl
 		'smtp_from_name' => 'Community Memory',
